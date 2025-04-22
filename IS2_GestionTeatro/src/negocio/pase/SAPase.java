da461a099213c016ad34de3b61f03206096be4e4
JorgeSanPedro <sampesansol04@gmail.com>
2025-04-22 17:42:03 +0200

    metodo comprar y arreglos

--- IS2_GestionTeatro/src/negocio/pase/SAPase.java
+++ IS2_GestionTeatro/src/negocio/pase/SAPase.java
@@ -2,14 +2,16 @@ package negocio.pase;
 
 import java.util.Collection;
 
+import exceptions.UnknownCompTeaException;
+import exceptions.UnknownObraException;
+
 public interface SAPase {
-	public int create(TPase tPase);
+	public int create(TPase tPase) throws UnknownObraException, UnknownCompTeaException;
 	public TPase read(int id);
 	public int update(TPase tPase);
 	public int delete (int id);
 	public Collection<TPase> readAll();
-	public int comprar(int idPase, int cantidad); 
-	//Devuelve el stock comprado del pase con idPase. 
-	//Si cantidad > stock, entonces da todo lo que tenga (lo dijo Vicky)
-	//Además, actualiza en la BD el pase con dicho id
+	public int comprar(int idPase, int cantidad);  //Devuelve el stock comprado del pase con idPase. 
+												   //Si cantidad > stock, entonces da todo lo que tenga (lo dijo Vicky)
+												   //Además, actualiza en la BD el pase con dicho id
 }
