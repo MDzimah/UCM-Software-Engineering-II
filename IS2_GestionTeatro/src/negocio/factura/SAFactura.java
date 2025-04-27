d6d5cedf90275ba5ae25fa93ab2334f927d02068
MDzimah <manukwabe3452@gmail.com>
2025-04-27 23:37:56 +0200

    Cambios menores

--- IS2_GestionTeatro/src/negocio/factura/SAFactura.java
+++ IS2_GestionTeatro/src/negocio/factura/SAFactura.java
@@ -5,9 +5,9 @@ import java.util.Collection;
 import exceptions.*;
 
 public interface SAFactura {
-	public int crearFactura(TDatosVenta tDv) throws UnknownClienteException, UnknownTaquilleroException, BBDDReadException, BBDDWriteException;
 	public int anyadirPaseAVenta(TLineaFactura newTLf, Collection<TLineaFactura> carrito) throws BBDDReadException;
 	public int quitarPaseDeVenta(TLineaFactura tLfAQuitar, Collection<TLineaFactura> carrito) throws BBDDReadException;
+	public int crearFactura(TDatosVenta tDv) throws UnknownClienteException, UnknownTaquilleroException, BBDDReadException, BBDDWriteException;
 	public TFactura buscarFactura(int id) throws BBDDReadException;
 	public Collection<TFactura> facturasActivas() throws BBDDReadException;
 	public Collection<TFactura> facturasPorCliente(int idCliente) throws BBDDReadException ;
--- IS2_GestionTeatro/src/negocio/factura/SAFacturaImp.java
+++ IS2_GestionTeatro/src/negocio/factura/SAFacturaImp.java
@@ -26,6 +26,65 @@ import presentacion.GUIFactura.VistaVentaEnCurso;
 import presentacion.factoria.FactoriaAbstractaPresentacion;
 
 public class SAFacturaImp implements SAFactura {
+
+	@Override
+	public int anyadirPaseAVenta(TLineaFactura newTLf, Collection<TLineaFactura> carrito) throws BBDDReadException {
+		SAPase saP = FactoriaAbstractaNegocio.getInstance().crearSAPase();
+		
+		TPase tPase = saP.read(newTLf.getIdPase());
+		
+		boolean estaba = false;
+		
+		if (tPase != null) {
+			Collection<TLineaFactura> carr = VistaVentaEnCurso.getCarrito();
+			
+			//Recorremos el carrito para ver si ya hay una instancia de la línea factura
+			//Si sí, entonces sumamos a la cantidad que ya había (asegurando que queda stock)
+			for(TLineaFactura tLf : carr) {
+				if (tLf.getIdPase() == newTLf.getIdPase()) {
+					if (tPase.getStock() - tLf.getCantidad() - newTLf.getCantidad() >= 0) tLf.setCantidad(tLf.getCantidad() + newTLf.getCantidad());
+					else return -1;
+					estaba = true;
+					break;
+				}
+			}
+			
+			//No hay instancia previa de newTLf en el carrito
+			if (!estaba) {
+				if (tPase.getStock() - newTLf.getCantidad() >= 0) carrito.add(newTLf);	
+				else return -1;
+			}
+			
+			return 1;
+		}
+		else return 0;
+	}
+	
+	@Override
+	public int quitarPaseDeVenta(TLineaFactura tLfAQuitar, Collection<TLineaFactura> carrito) throws BBDDReadException {
+		SAPase saP = FactoriaAbstractaNegocio.getInstance().crearSAPase();
+		
+		TPase tPase = saP.read(tLfAQuitar.getIdPase());
+		
+		if (tPase != null) {
+			ArrayList<TLineaFactura> carr = (ArrayList<TLineaFactura>) VistaVentaEnCurso.getCarrito();
+			
+			boolean estaba = false;
+			for(int i = 0; i < carr.size(); ++i) {
+				TLineaFactura tLf = carr.get(i);
+				if (tLf.getIdPase() == tLfAQuitar.getIdPase()) {
+					tLf.setCantidad(tLf.getCantidad() - tLfAQuitar.getCantidad());
+					if (carr.get(i).getCantidad() <= 0) carr.remove(i);
+					estaba = true;
+					break;
+				}
+			}
+				
+			if (estaba) return 1;
+			else return -1;
+		}
+		else return 0;
+	}
 	
 	public int crearFactura(TDatosVenta tDv) throws UnknownClienteException, UnknownTaquilleroException, BBDDReadException, BBDDWriteException  {
 		int idFacNueva = -1;
@@ -91,65 +150,6 @@ public class SAFacturaImp implements SAFactura {
 		}
 		
 		return idFacNueva;
-	}
-
-	@Override
-	public int anyadirPaseAVenta(TLineaFactura newTLf, Collection<TLineaFactura> carrito) throws BBDDReadException {
-		SAPase saP = FactoriaAbstractaNegocio.getInstance().crearSAPase();
-		
-		TPase tPase = saP.read(newTLf.getIdPase());
-		
-		boolean estaba = false;
-		
-		if (tPase != null) {
-			Collection<TLineaFactura> carr = VistaVentaEnCurso.getCarrito();
-			
-			//Recorremos el carrito para ver si ya hay una instancia de la línea factura
-			//Si sí, entonces sumamos a la cantidad que ya había (asegurando que queda stock)
-			for(TLineaFactura tLf : carr) {
-				if (tLf.getIdPase() == newTLf.getIdPase()) {
-					if (tPase.getStock() - tLf.getCantidad() - newTLf.getCantidad() >= 0) tLf.setCantidad(tLf.getCantidad() + newTLf.getCantidad());
-					else return -1;
-					estaba = true;
-					break;
-				}
-			}
-			
-			//No hay instancia previa de newTLf en el carrito
-			if (!estaba) {
-				if (tPase.getStock() - newTLf.getCantidad() >= 0) carrito.add(newTLf);	
-				else return -1;
-			}
-			
-			return 1;
-		}
-		else return 0;
-	}
-	
-	@Override
-	public int quitarPaseDeVenta(TLineaFactura tLfAQuitar, Collection<TLineaFactura> carrito) throws BBDDReadException {
-		SAPase saP = FactoriaAbstractaNegocio.getInstance().crearSAPase();
-		
-		TPase tPase = saP.read(tLfAQuitar.getIdPase());
-		
-		if (tPase != null) {
-			ArrayList<TLineaFactura> carr = (ArrayList<TLineaFactura>) VistaVentaEnCurso.getCarrito();
-			
-			boolean estaba = false;
-			for(int i = 0; i < carr.size(); ++i) {
-				TLineaFactura tLf = carr.get(i);
-				if (tLf.getIdPase() == tLfAQuitar.getIdPase()) {
-					tLf.setCantidad(tLf.getCantidad() - tLfAQuitar.getCantidad());
-					if (carr.get(i).getCantidad() <= 0) carr.remove(i);
-					estaba = true;
-					break;
-				}
-			}
-				
-			if (estaba) return 1;
-			else return -1;
-		}
-		else return 0;
 	}
 	
 	@Override
