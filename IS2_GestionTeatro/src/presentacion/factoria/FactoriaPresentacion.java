48f1b4b612932b47b8921e06946ac29dc1072214
MDzimah <manukwabe3452@gmail.com>
2025-05-01 14:08:26 +0200

    Correción FacPresentación

--- IS2_GestionTeatro/src/presentacion/factoria/FactoriaPresentacion.java
+++ IS2_GestionTeatro/src/presentacion/factoria/FactoriaPresentacion.java
@@ -11,7 +11,6 @@ import presentacion.GUIObra.VistaConsultarObra;
 import presentacion.GUIObra.VistaMostrarObras;
 import presentacion.GUIPase.VistaActualizarPase;
 import presentacion.GUIPase.VistaBuscarPase;
-import presentacion.GUIPase.VistaConsultarPase;
 import presentacion.GUIPase.VistaCrearPase;
 import presentacion.GUIPase.VistaEliminarPase;
 import presentacion.GUIPase.VistaListarPases;
@@ -22,7 +21,6 @@ public class FactoriaPresentacion extends FactoriaAbstractaPresentacion {
 	@Override
 	public IGUI createVista(Evento e) {
 		switch(e) {
-		case MAINWINDOW: return new MainWindow();
 		//Factura
 		case ANYADIR_PASE_A_VENTA: return new VistaAnyadirPaseAVenta();
 		case BUSCAR_FACTURA: return new VistaBuscarFac();
